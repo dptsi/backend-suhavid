78650b354e0af5d26ad4ff7da918b5a49c47a684
Suhavid Hendra <suhavidhendra@bitbucket.org>
2023-05-09 14:55:41 +0700

    golang fundamental - conditional, for, array, slice

--- main.go
+++ main.go
@@ -8,7 +8,107 @@ import (
 func main() {
 	fmt.Println("Halo, suhavid hendra")
 
-	result := calculation.Add(10, 9)
+	result := calculation.Add(10, 9) // tanda := untuk membuat variabel baru tanpa menyertakan tipe data
 
 	fmt.Println(result)
+
+	//	If conditional
+	score := 30
+	var grade string
+
+	if score <= 50 {
+		grade = "E"
+	} else if score <= 60 {
+		grade = "D"
+	} else if score < 70 {
+		grade = "C"
+	} else {
+		grade = "NULL"
+	}
+
+	fmt.Println(grade)
+
+	// Switch conditional
+	number := 5
+
+	switch number {
+	case 1:
+		fmt.Println("Satu")
+	case 2:
+		fmt.Println("Dua")
+	case 3:
+		fmt.Println("Tiga")
+	default:
+		fmt.Println("Default")
+	}
+
+	//Perulangan for
+
+	//for biasa
+	for i := 1; i <= 100; i++ {
+		fmt.Println("mencoba perulangan Go :", i)
+	}
+
+	// while
+	i := 1
+	for i <= 100 {
+		fmt.Println("mencoba perulangan Go :", i)
+		i++
+	}
+
+	// foreach
+	title := "Golang the best language"
+	for index, letter := range title {
+		fmt.Println("index :", index, " letter ", string(letter))
+	}
+
+	// jika index tidak digunakan
+	for _, letter := range title {
+		fmt.Println(" letter :", string(letter))
+	}
+
+	// for dengan kondisi
+	for index, letter := range title {
+		if index%2 == 0 {
+			fmt.Println("index :", index, " letter ", string(letter))
+		}
+	}
+
+	// for dengan kondisi while
+	for index, letter := range title {
+		letterString := string(letter)
+
+		switch letterString {
+		case "a", "i", "u", "e", "o":
+			fmt.Println("index :", index, " letter ", string(letter))
+		}
+	}
+
+	// array for
+	languages := [...]string{
+		"Ruby",
+		"C++",
+		"PHP",
+		"GO",
+	}
+
+	for index, lang := range languages {
+		fmt.Println("Index : ", index, " language : ", lang)
+	}
+
+	length := len(languages)
+	fmt.Println(length)
+
+	// slice
+	var gamingConsole []string
+	gamingConsole = append(gamingConsole, "PlayStation5")
+	gamingConsole = append(gamingConsole, "PlayStation2")
+	fmt.Println(gamingConsole)
+
+	gaming := []string{"playstation2", "xbox"}
+	fmt.Println(gaming)
+
+	for _, console := range gaming {
+		fmt.Println(console)
+	}
 }
